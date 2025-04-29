62bb3d3d7dab8810b74b165bf6e8d83183ddd0e8
kendrickhn <harrisonrogger@gmail.com>
2025-04-29 18:15:34 +1000

    Update

---
+++ problems/easy/lc1327_list_the_products_ordered_in_a_period_self.sql
@@ -0,0 +1,9 @@
+-- solved in 7:25
+select p.product_name, sum(o.unit) as unit 
+from Products p
+right join Orders o
+on p.product_id = o.product_id
+where o.order_date >= "2020-02-01" and o.order_date < "2020-03-01"
+group by p.product_id
+having sum(o.unit) >= 100
+
