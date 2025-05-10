c9d1ab310c18810003a7c502b3e005bb0c3713ee
kendrickhn <harrisonrogger@gmail.com>
2025-05-10 10:46:04 +1000

    Update

---
+++ problems/medium/lc1158_market_analysis_I_self.sql
@@ -0,0 +1,6 @@
+select u.user_id as buyer_id, u.join_date, ifnull(count(o.item_id),0) as 'orders_in_2019'
+from Orders o 
+right join Users u 
+on o.buyer_id=u.user_id and YEAR(o.order_date)= '2019'
+group by u.user_id 
+
