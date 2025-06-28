b8572dfd0cd845a8e10e4b76bd1aead200b09af6
kendrickhn <harrisonrogger@gmail.com>
2025-06-28 12:55:30 +1000

    Update

---
+++ problems/easy/lc1_two_sum_self.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for c in range(len(nums)):
+            b = target - nums[c]
+            a = []
+            a.append(c)
+            if b in nums and nums.index(b) != c:
+                a.append(nums.index(b))
+                return a
+            
+                
